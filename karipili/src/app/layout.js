1ad2d4bb7bbbe21ab3559910c41f1936e3afb355
Reza Fahlevi Herdiyanto <rfahlevih@gmail.com>
2024-06-29 02:37:43 +0700

    Update layout.js
    add meta

--- karipili/src/app/layout.js
+++ karipili/src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <head>
+        <meta httpEquiv="Access-Control-Allow-Origin" content="*" />
+      </head>
       <body className={poppins.className}>
         {children}
         <Toaster position="top-center" richColors />
